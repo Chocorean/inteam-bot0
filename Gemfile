201ecc762029bfa150584f275886febe2fcd7467
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2020-04-02 05:55:12 +0000

    Add random French word gen

--- Gemfile
+++ Gemfile
@@ -5,5 +5,6 @@ source 'https://rubygems.org'
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'discordrb'
+gem 'nokogiri'
 gem 'opus-ruby'
 gem 'rbnacl'
--- src/commands/poireau.rb
+++ src/commands/poireau.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+require 'open-uri'
+require 'nokogiri'
+
+def get_word
+  data = Nokogiri::HTML.parse(open('https://www.palabrasaleatorias.com/mots-aleatoires.php'))
+  data.search('//tr//td//div').inner_text[2..-1]
+end
+
+
 module Bot
   module DiscordCommands
     # Goes into main vocal channel and shouts a random song.
@@ -9,12 +18,17 @@ module Bot
         # Joining user channel
         channel = event.user.voice_channel
         next CONFIG.i18n['poireau_no_channel'] unless channel
-        BOT.voice_connect(channel)
-        # Forging Youtube URL search
-        webpage = `curl https://www.palabrasaleatorias.com/mots-aleatoires.php`
-        puts webpage
-        #url = "https://www.youtube.com/results?search_query=#{}"
-        #voice()
+        # Getting random French word
+	data = Nokogiri::HTML.parse(open('https://www.palabrasaleatorias.com/mots-aleatoires.php'))
+        random_word = data.search('//tr//td//div').inner_text[2..-1]
+	# Forging Youtube search request
+	# TODO
+	return nil
+	# Joining voice channel and shouting
+        # TODO
+	# BOT.voice_connect(channel)
+	# BOT.voice()
+	# etc
       end
     end
   end
